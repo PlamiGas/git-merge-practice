cebbbbb02db3aaa897f2eee2c0eace1e93b3fefe
PlamiGas <plam28714@gmail.com>
2026-03-26 11:16:39 +0500

    Добавлена базовые функции калькулятора(add, subtract)

---
+++ calculator.py
@@ -0,0 +1,11 @@
+#calculator.py
+def add(a, b):
+    return a+b
+
+def subtract(a, b):
+    return a-b
+
+if ___name___=="___main___":
+    print("Простой калькулятор")
+    print("Сложение 5+3=", add(5, 3))
+    print("Вычитание 10-4=", substract(10, 4))
